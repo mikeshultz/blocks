4b86142bade01b9584061dcd6419c40c6b4b8dea
Mike Shultz <shultzm@gmail.com>
2017-12-27 21:06:02 -0700

    Fixes to datatypes

--- relationalblocks/sql/initial.sql
+++ relationalblocks/sql/initial.sql
@@ -1,27 +1,27 @@
 CREATE TABLE block (
     block_number serial PRIMARY KEY,
     block_timestamp timestamp without time zone NOT NULL,
-    nonce integer NOT NULL,
-    difficulty numeric(19) NOT NULL,
-    size integer NOT NULL,
-    gas_limit integer NOT NULL,
-    gas_used numeric(19) NOT NULL,
     hash varchar(66) NOT NULL,
-    miner varchar(42) NOT NULL
+    miner varchar(42) NOT NULL,
+    nonce numeric NOT NULL,
+    difficulty numeric NOT NULL,
+    gas_used numeric NOT NULL,
+    gas_limit integer NOT NULL,
+    size integer NOT NULL
 );
 CREATE INDEX block__block_timestamp ON block (block_timestamp);
-CREATE INDEX block__hash ON block (block_hash);
-CREATE INDEX block__miner ON block (block_miner);
+CREATE INDEX block__hash ON block (hash);
+CREATE INDEX block__miner ON block (miner);
 
 CREATE TABLE transaction (
     hash varchar(66) PRIMARY KEY,
     block_number integer REFERENCES block(block_number) NOT NULL,
-    nonce integer NOT NULL,
     from_address varchar(42) NOT NULL,
     to_address varchar(42) NOT NULL,
-    value numeric(19) NOT NULL,
-    gas_price numeric(19) NOT NULL,
+    value numeric NOT NULL,
+    gas_price numeric NOT NULL,
     gas_limit integer NOT NULL,
+    nonce integer NOT NULL,
     input varchar
 );
 CREATE INDEX transaction__block_number ON transaction (block_number);
