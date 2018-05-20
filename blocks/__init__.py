c2be234eeddaa6d9c0739cd15e428e0ec741c7be
Mike Shultz <shultzm@gmail.com>
2018-05-19 23:31:49 -0600

    Setting up a health check since API won't be part of this container.

---
+++ blocks/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+"""
+So, the entire point of the flask app is to satisfy ECS health checks.  There's 
+no reason this is necessary for the consumer to function.  
+TODO: Perhaps this can be done away with somehow?
+TODO: Or at least make this health check a little more useful?
+"""
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+def api():
+    """ Run the debug server """
+    app.run(port=8080)
